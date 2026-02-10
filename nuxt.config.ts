6ccef13c1f9b9ea4b4d185d1ac28777a558b6916
RomualdAKM <hope91303142@gmail.com>
2026-02-10 08:34:09 +0100

    meta data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,28 @@ export default defineNuxtConfig({
   ssr: false,
   app: {
     head: {
+      title: 'Uber-Market - Créez votre boutique en ligne en 1m30s',
+      htmlAttrs: {
+        lang: 'fr'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'Uber-Market, une plateforme intuitive et clé en main qui vous permet de créer ou de développer votre activité en ligne sans aucune connaissance en programmation.' },
+        { name: 'keywords', content: 'créer boutique en ligne, e-commerce, site web, marketplace, sans code, solution clé en main, uber-market' },
+        { property: 'og:site_name', content: 'Uber-Market' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:title', content: 'Uber-Market - Créez votre boutique en ligne en 1m30s' },
+        { property: 'og:description', content: 'Uber-Market, une plateforme intuitive et clé en main qui vous permet de créer ou de développer votre activité en ligne sans aucune connaissance en programmation.' },
+        { property: 'og:image', content: 'https://www.uber-market.com/uber-market.png' },
+        { property: 'og:url', content: 'https://www.uber-market.com' },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:title', content: 'Uber-Market - Créez votre boutique en ligne en 1m30s' },
+        { name: 'twitter:description', content: 'Uber-Market, une plateforme intuitive et clé en main qui vous permet de créer ou de développer votre activité en ligne sans aucune connaissance en programmation.' },
+        { name: 'twitter:image', content: 'https://www.uber-market.com/uber-market.png' }
+      ],
       link: [
+        { rel: 'icon', type: 'image/png', href: '/uber-market.png' },
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
